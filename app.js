f6cde9d252908ed760446a04168c0b2d0f0b5e0d
Ronald Karamuca <11005050@live.mercer.edu>
2021-04-19 22:41:59 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#number-form");
+const response = document.querySelector("#message");
+const advert = document.querySelector("#ad");
+
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    let x = form.elements.query.value;
+    if (isNaN(x)) {
+        response.innerText = `${x} is not a number!`;
+        form.elements.query.value = "";
+        advert.innerText = "";
+        return;
+    }
+    try {
+        const result = await fetch(
+            `https://api.isevenapi.xyz/api/iseven/${x}/`
+        );
+        const data = await result.json();
+        if (data.iseven) {
+            response.innerText = `${x} is even!`;
+        } else {
+            response.innerText = `${x} is odd :(`;
+        }
+        advert.innerText = `Ad: ${data.ad}`;
+    } catch (e) {
+        response.innerText = ("Something went wrong:", e);
+    }
+    form.elements.query.value = "";
+});
